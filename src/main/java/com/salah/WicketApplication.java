29369348f66734743bb9e910856e6f148c7d1724
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-07-21 02:56:54 +0300

    The application class

--- src/main/java/com/salah/WicketApplication.java
+++ src/main/java/com/salah/WicketApplication.java
@@ -8,34 +8,33 @@ import org.apache.wicket.protocol.http.WebApplication;
 /**
  * Application object for your web application.
  * If you want to run this application without deploying, run the Start class.
- * 
- * @see com.salah.Start#main(String[])
+ *
+ * This class is the value of parameter applicationClassName in web.xml
  */
-public class WicketApplication extends WebApplication
-{
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
-	@Override
-	public Class<? extends WebPage> getHomePage()
-	{
-		return HomePage.class;
-	}
+public class WicketApplication extends WebApplication {
+    /**
+     * @see org.apache.wicket.Application#getHomePage()
+     * Defining the home page, Duh!
+     */
+    @Override
+    public Class<? extends WebPage> getHomePage() {
+        return HomePage.class;
+    }
 
-	/**
-	 * @see org.apache.wicket.Application#init()
-	 */
-	@Override
-	public void init()
-	{
-		super.init();
+    /**
+     * @see org.apache.wicket.Application#init()
+     * This method is called when our application is loaded by the web server  and is the ideal place to put our configuration code.
+     */
+    @Override
+    public void init() {
+        super.init();
 
-		// needed for the styling used by the quickstart
-		getCspSettings().blocking()
-			.add(CSPDirective.STYLE_SRC, CSPDirectiveSrcValue.SELF)
-			.add(CSPDirective.STYLE_SRC, "https://fonts.googleapis.com/css")
-			.add(CSPDirective.FONT_SRC, "https://fonts.gstatic.com");
+        // needed for the styling used by the quickstart
+        getCspSettings().blocking()
+                .add(CSPDirective.STYLE_SRC, CSPDirectiveSrcValue.SELF)
+                .add(CSPDirective.STYLE_SRC, "https://fonts.googleapis.com/css")
+                .add(CSPDirective.FONT_SRC, "https://fonts.gstatic.com");
 
-		// add your configuration here
-	}
+        // add your configuration here
+    }
 }
