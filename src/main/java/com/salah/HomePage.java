5e48f220f4f3d80ec0cf88c3bff99d92fec4c2ae
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-07-21 03:10:19 +0300

    HomePage

--- src/main/java/com/salah/HomePage.java
+++ src/main/java/com/salah/HomePage.java
@@ -5,14 +5,17 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends WebPage {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public HomePage(final PageParameters parameters) {
-		super(parameters);
+    public HomePage(final PageParameters parameters) {
+        super(parameters);
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+        // add: used to add children components to a web page
+        // here is some data binding type shit
+        add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+        add(new Label("helloMessage", "Hello WicketWorld!"));
 
-		// TODO Add your page's components here
+        // TODO Add your page's components here
 
-	}
+    }
 }
